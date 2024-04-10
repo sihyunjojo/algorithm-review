edbed95b15b474638ef3fd5842cf411d9fb1f0ba
sihyunjojo <87458171+sihyunjojo@users.noreply.github.com>
2024-04-10 21:26:27 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 580 ms, Memory: 121612 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.java
@@ -0,0 +1,52 @@
+import javax.swing.text.html.ListView;
+import java.awt.desktop.QuitEvent;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        final int MIN = -1000000000;
+//        System.out.println(MIN);
+        int[] arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken()) - MIN;
+        }
+
+        int[] list = new int[n];
+        list[0] = arr[0];
+
+        int res = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (list[res] < arr[i]) {
+                list[++res] = arr[i];
+            } else {
+                int left = 0;
+                int right = res;
+
+                while (left < right){
+                    int mid = (left + right) / 2;
+                    // 괜찮은 경우
+                    if (arr[i] > list[mid]){
+                        left = mid +1;
+                    } else {
+                        right = mid;
+                    }
+                }
+                list[right] = arr[i];
+            }
+//            System.out.println(Arrays.toString(list));
+        }
+
+        System.out.println(res+1);
+
+    }
+}
