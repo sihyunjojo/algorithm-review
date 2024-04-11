2b95202082a0bd2a515ce41e6d0d7874a9de8c84
sihyunjojo <87458171+sihyunjojo@users.noreply.github.com>
2024-04-11 11:45:41 +0900

    [Gold III] Title: 소수의 연속합, Time: 192 ms, Memory: 26996 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
@@ -1,42 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-public class Main {
 
-    static boolean primeflag[];
-    static ArrayList<Integer> prime;
-    static int subtotal[];
+public class Main {
+    static int start,end,sum,res;
+    static boolean[] dp;
+    static ArrayList<Integer> arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        prime = new ArrayList<>();
-        makePrime(N);
+        int n = Integer.parseInt(br.readLine());
 
-        int start=0, end=0, sum=0, count=0;
-        while(true) {
-            if(sum >= N ) sum -= prime.get(start++);
-            else if(end == prime.size()) break;
-            else sum += prime.get(end++);
-            if(N==sum) count++;
+        arr = new ArrayList<>();
+        dp = new boolean[n + 1];
+        dp[0] = dp[1] = true;
+        makePrime(n);
+
+        while (true) {
+            if (n <= sum) sum -= arr.get(start++);
+            else if (end == arr.size()) break;
+            else sum += arr.get(end++);
+            if (sum == n) res++;
         }
-        System.out.println(count);
 
-
+        System.out.println(res);
     }
-    public static void makePrime(int n) {
-        primeflag = new boolean[n + 1];
-        primeflag[0] = primeflag[1] = true;
-        for(int i=2; i*i<=n; i++){
-            // prime[i]가 소수라면
-            if(!primeflag[i]){
-                // prime[j] 소수가 아닌 표시
-                for(int j=i*i; j<=n; j+=i) primeflag[j] = true;
+
+    private static void makePrime(int n) {
+        for (int i = 2; i * i <= n; i++) {
+            if (!dp[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    dp[j] = true;
+                }
             }
         }
-        for (int i = 1; i <=n; i++)
-            if (!primeflag[i]) prime.add(i);
+
+        for (int i = 2; i <= n; i++) {
+            if (!dp[i]) arr.add(i);
+        }
+
     }
 }
