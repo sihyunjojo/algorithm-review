deeb312b1a76b8feaec19678e420672d74bd04dc
sihyunjojo <87458171+sihyunjojo@users.noreply.github.com>
2024-04-13 23:14:14 +0900

    [Bronze V] Title: 오늘 날짜, Time: 116 ms, Memory: 14008 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        System.out.println("2024-04-13");
+    }
+}
